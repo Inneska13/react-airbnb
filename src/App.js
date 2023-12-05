383c01e5ed69f60aeaef84ddb214158953244833
Inneska13 <innaivanchuk1313@gmail.com>
2023-12-05 16:08:14 +0200

    upd

--- src/App.js
+++ src/App.js
@@ -1,3 +1,12 @@
+import Page from "./component/page";
+import Header from "./component/header";
+import Title from "./component/title";
+import Photo from "./component/photo";
+import Price from "./component/price";
+import RoomList from "./component/room-list";
+import Description from "./component/description";
+import Details from "./component/details";
+
 function App() {
   const data = {
     listing_name: "Іст-Сайд Біл",
@@ -144,7 +153,33 @@ function App() {
     ],
   };
 
-  return <div>Hello World</div>;
+  return (
+    <Page>
+      <Header />
+      <Title
+        title={data.listing_name}
+        rating={data.reviews_summary.average_rating}
+        review={data.reviews_summary.total_reviews}
+        city={data.location.city}
+        country={data.location.country}
+        superhost={data.superhost}
+      />
+      <Photo src={data.image} name={data.listing_name} />
+      <Price
+        price={data.price.original_price}
+        discount={data.price.discounted_price}
+        currency={data.price.currency}
+        cleaning={data.price.cleaning_fee}
+        service={data.price.service_fee}
+        checkin={data.availability.checkin_date}
+        checkout={data.availability.checkout_date}
+      />
+      <RoomList list={data.roomTypes} />
+      <Description title="Oпис" children={data.description} />
+      <Description title="Про сусідів" children={data.neighborhood_info} />
+      <Details children={data.details} />
+    </Page>
+  );
 }
 
 export default App;
---
+++ src/component/details/index.js
@@ -0,0 +1,58 @@
+import "./index.css";
+import Heading from "../heading";
+import Box from "../box";
+import ListItem from "../list-item";
+import test from "./test.svg";
+import bed from "./bed.svg";
+import bedroom from "./bedroom.svg";
+
+export default function Details({ guests, bedrooms, beds, ...rest }) {
+  return (
+    <Box shadow className="price">
+      <Heading border>Деталі властивості:</Heading>
+      <div class="price__header">
+        <span className="price__value">{guests}</span>
+
+        <span className={`price__value`}>{bedrooms}</span>
+      </div>
+      <List {...rest} beds={beds} />
+    </Box>
+  );
+}
+
+function List({ guests, bedrooms, beds, beths }) {
+  return (
+    <ul className="price__list">
+      <ListItem>
+        <span>
+          <img src={test} alt="Test" height={24} />
+          {guests}
+        </span>
+        <span>гості </span>
+      </ListItem>
+
+      <ListItem>
+        <span>
+          <img src={bedroom} alt="Bedroom" height={24} />
+          {bedrooms}
+        </span>
+        <span>спальня</span>
+      </ListItem>
+
+      <ListItem>
+        <span>
+          <img src={bed} alt="Bed" height={24} />
+          {beds}
+        </span>
+        <span>ліжко</span>
+      </ListItem>
+
+      <ListItem>
+        <span>
+          <img src={bedroom} alt="Beths" height={24} /> {beths}
+        </span>
+        <span> ванна кімната</span>
+      </ListItem>
+    </ul>
+  );
+}
